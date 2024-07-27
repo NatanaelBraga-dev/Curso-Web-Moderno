c5fcfd0ed395d3c3cea223e2a5d40adbf8dec8f9
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2024-07-27 13:17:17 -0300

    Create notacaoLiteral.js
    estudando melhorias do ES 2015 em relação aos objetos

---
+++ Curso Web Moderno/objetos/notacaoLiteral.js
@@ -0,0 +1,30 @@
+//melhorias do Ecma Script 2015
+
+const a = 1
+const b = 2
+const c = 3
+
+const obj1 = {a: a, b: b, c: c}
+const obj2 = {a,b,c} //não precisa mais passar os valores como feito acima
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.89
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3);
+
+const obj4 = {[nomeAttr]: valorAttr}
+console.log(obj4);
+
+const obj5 = {
+    funcao1: function(){
+        //...
+    },
+    funcao2: function(){
+        //...
+    }
+}
+
+console.log(obj5);
