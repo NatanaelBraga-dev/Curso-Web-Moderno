d382ebfc15c2345710962c666bb33458dca494e9
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2024-08-04 18:31:51 -0300

    Create heranca2.js
    estudando cadeias de protótipos utilizando exemplos com atributos e métodos

---
+++ Curso Web Moderno/objetos/heranca2.js
@@ -0,0 +1,53 @@
+//cadeia de protótipos (prototype chain)
+
+const avo = {attr1: 'A'}
+const pai = {__proto__:avo, attr2: 'B'}
+const filho = {__proto__: pai, attr3: 'C'}
+console.log(filho.attr1);//vai procurar nos protótipos até achar 
+console.log(filho.attr0);
+
+
+//__proto__: 'filho' (dessa forma está definido que o protótipo desse objeto é o objeto filho)
+
+const carro ={
+    velAtual: 0,
+    velMax: 200,
+    acelerarMais(delta){
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        }
+        else{
+            this.velAtual = this.velMax
+        }
+    },
+
+    status(){
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 //sobrescreve o velMax de carro 
+
+}
+
+const volvo = {
+    modelo : 'V40',
+    status(){
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari,carro)//essa função torna 'carro' o protótipo de ferrari 
+Object.setPrototypeOf(volvo,carro)
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.acelerarMais(100)
+console.log(volvo.status());
+
+ferrari.acelerarMais(300)
+console.log(ferrari.status());
+
