b65cb311fc41182dddd230e987e4319aedd13102
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2024-12-31 19:38:25 -0300

    aprendendo a utilizar o metodo concat

---
+++ Curso Web Moderno/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['valeska', 'Cibalena']
+const filhos =['Washignton', 'Wesclei']
+
+const todos = filhas.concat(filhos,'fulano')
+console.log(filhas, filhos, todos);
+
+console.log([].concat([1,2], [3,4], 5, [[6,7]]));
