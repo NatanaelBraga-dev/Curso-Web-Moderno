35904361088a27a30c258bfc2b4c00d55d0a6450
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2024-12-31 19:12:31 -0300

    aprendendo sobre programação imperativa e declarativa

---
+++ Curso Web Moderno/array/imperativoVsDeclarativo.js
@@ -0,0 +1,21 @@
+const alunos = [
+    {nome: 'João', nota: 7.9},
+    {nome: 'Maria', nota: 9.2}
+]
+
+//imperativo
+let total1 = 0
+for (let i = 0; i < alunos.length; i++){
+    total1 += alunos[i].nota
+}
+console.log(total1/alunos.length);
+
+//declarativo
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+
+const total2 = alunos.map(getNota).reduce(soma)
+console.log(total2 / alunos.length);
+
+//o modo declarativo se importa menos com o como e mais com o que estamos fazendo
+//os processos minusiosos são reduzidos
