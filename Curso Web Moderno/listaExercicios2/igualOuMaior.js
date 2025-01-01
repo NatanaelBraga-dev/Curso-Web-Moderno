1b58c330685e094e8b15b1b24cf08e48d173b4b8
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-01 18:10:02 -0300

    função que retorna se o primeiro numero é maior ou igual ao segundo

---
+++ Curso Web Moderno/listaExercicios2/igualOuMaior.js
@@ -0,0 +1,7 @@
+/**
+ * crie uma função que recebe dois numeros e retorne se o primeiro é maior ou igual ao segundo
+ */
+
+const maiorOuIgual = (numero1, numero2) => numero1 === numero2 || numero1 > numero2
+console.log(maiorOuIgual(1,1));
+console.log(maiorOuIgual(1,"1"));
