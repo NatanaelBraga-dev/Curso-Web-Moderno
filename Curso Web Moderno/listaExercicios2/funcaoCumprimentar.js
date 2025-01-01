61bdcd77ad181d7f80da9205ad3ac5febf34d1e8
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-01 17:26:17 -0300

    exercicio função cumprimentar

---
+++ Curso Web Moderno/listaExercicios2/funcaoCumprimentar.js
@@ -0,0 +1,13 @@
+/**
+ * crie uma função que retorna a string "olá" concatenada com um argumento text passado pela função e com ponto "!"
+ * no final 
+ * 
+ * Exemplo: 
+ * cumprimentar("Leonardo") retorna "Olá Leonardo!"
+ */
+
+const cumprimentar = function(text){
+    return console.log('Olá', text,'!');
+}
+
+cumprimentar("Leonardo")
