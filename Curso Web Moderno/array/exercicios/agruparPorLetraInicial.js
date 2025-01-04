49a392688ae1938476307c7993f4c8c2ebb394c5
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-04 20:31:20 -0300

    desafio de ordenar as palavras de acordo com a letra que ela começa usando forEach

---
+++ Curso Web Moderno/array/exercicios/agruparPorLetraInicial.js
@@ -0,0 +1,26 @@
+/**
+ *  Crie uma função que receba um array de strings e retorne um objeto onde as chaves
+ *  sejam as primeiras letras de cada palavra, e os valores sejam arrays contendo as palavras
+ *  que começam com aquela letra. Use forEach
+ */
+
+const words = ["apple","banana","cherry","avocado","blueberry"];
+let letrasIniciais = []
+words.forEach(function(nome,indice){
+    if(letrasIniciais.includes(nome[0])){
+
+    }else{
+        letrasIniciais.push(nome[0])
+    }
+})
+
+letrasIniciais.forEach(function(letra, indice){
+    let palavrasDaRespectivaLetra = []
+    let somenteEssaLetra = words.filter(function(nomes, indices){            
+            if (nomes[0] === letra){
+                palavrasDaRespectivaLetra.push(nomes)
+            }
+    })
+    return console.log(`${letra}: ${palavrasDaRespectivaLetra}`);    
+})
+
