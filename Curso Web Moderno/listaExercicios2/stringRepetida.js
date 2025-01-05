154e445dc9253941325232af072fb8df5c4f704e
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-05 16:42:03 -0300

    função que retorna um caracter de acordo com a qttd de vezes especificada

---
+++ Curso Web Moderno/listaExercicios2/stringRepetida.js
@@ -0,0 +1,10 @@
+/**
+ * elabore uma função que recebe um numero como parametro e retorne o simbolo '+'
+ * repetidamente de acordo com o numero repassado
+ */
+
+function repetirCaracter(numero){
+    return '+'.repeat(numero)
+}
+
+console.log(repetirCaracter(3));
