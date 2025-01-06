ee666465bf7f4b94b8f197af6a2f8368eb5e581c
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-06 20:29:13 -0300

    aprendendo sobre o sistema de modulos no Node utilizando CommonJS

---
+++ Curso Web Moderno/nodejs/moduloA.js
@@ -0,0 +1,5 @@
+//3 formas de exportar algo
+this.ola = "Fala Pessoal"
+exports.bemVindo = "Bem vindo ao node"
+module.exports.atelogo = "Até o próximo exemplo"
+
---
+++ Curso Web Moderno/nodejs/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA') //forma de importar o que está no moduloA.js
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.atelogo);
+console.log(moduloA);
+
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
+console.log(moduloB);
