76ab4f3ec981ae980ef0c9b1d8920e5349dc6b8a
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-07 08:57:41 -0300

    atualização sobre o uso do underline como variavel

--- Curso Web Moderno/nodejs/usandoModulosTerceiros.js
+++ Curso Web Moderno/nodejs/usandoModulosTerceiros.js
@@ -1,2 +1,2 @@
-const _ = require('lodash')
+const _ = require('lodash') // usei "_" para referenciar a biblioteca pois é uma convenção das pessoas que usam a biblioteca
 setInterval(() => console.log(_.random(1,1000)), 2000)
