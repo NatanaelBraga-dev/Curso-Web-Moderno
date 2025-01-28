ef78bd6e04eeb92d6c6d1373165afb9ef17f5e0b
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2025-01-28 19:13:29 -0300

    update promisses.js

--- Curso Web Moderno/esnext/promisses.js
+++ Curso Web Moderno/esnext/promisses.js
@@ -9,4 +9,4 @@ function falarDepoisDe(segundos, frase){
 falarDepoisDe(3,'Massa Demais')
     .then(frase => frase.concat('!!'))
     .then(frase => console.log(frase))
-    .catch(e => console.log(e))
+    .catch(e => console.log(e)) //chamada somente quando ocorrer um erro ou seja quando o reject for chamado pela promisse
